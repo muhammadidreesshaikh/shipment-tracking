cbb19b8fd3e0860eded082534d6717d1d47c078f
Muhammad Awais <awaisshaikh.official@gmail.com>
2020-10-05 12:24:39 +0500

    slider component done

--- src/shared/Slider.js
+++ src/shared/Slider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assets/css/slider.css';
-// import slider from '../assets/img/slider1.jpg'
+import shipment from '../assets/img/shipment-vector.png'
 
 class Slider extends React.Component {
 
@@ -25,7 +25,32 @@ class Slider extends React.Component {
         return(
             <section>
                 <div className="slider">
-                    <p>slider  slider  slider </p>
+                    <div className="container">
+                        <div className="row">
+
+                            <div className="col-12 col-md-6 col-lg-6">
+                                <div className="left">
+                                    <h1>Moving Made Fast & Sample</h1>
+                                    <p className="pt-2">We provide you the best service</p>
+
+                                    <div className="form-group pt-5">
+                                        <input type="Tracking Code" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Shipment Tracking Code"></input>
+                                    </div>
+
+                                    <div className="pt-3">
+                                        <a className="press" href="#">TRACK SHIPMENT</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-md-6 col-lg-6">
+                                <div className="right">
+                                    <img src={shipment} />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </section>
         );
