afffa5a336ded041c442ce2d726770739e253e9b
Muhammad Awais <awaisshaikh.official@gmail.com>
2020-10-05 22:53:53 +0500

    header and slider done with responsive

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import '../assets/css/header.css'
 // import logo from '../assets/img/logo_465x320.png'
 
 function Header() {
+
+    const [menuToggle, setMenuToggle] = useState(false);
+
+    console.log(menuToggle);
+
     return(
         <div>
+
+            
+
             <div className="header">
-                <div className="container">
+
+                <div className={"mobile-menu-toggle " + (menuToggle ? 'change' : '')} onClick={() => setMenuToggle(!menuToggle)}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
+
+                <div className="desktop-menu container">
                     <div className="row">
 
                         <div className="col-sm-4 col-md-4 col-lg-4">
@@ -31,6 +46,15 @@ function Header() {
                        
                     </div>
                 </div>
+
+                <div className={"mobile-menu sidenav " + (menuToggle ? 'open' : 'close')}>
+                    <Link to="/home" onClick={() => setMenuToggle(!menuToggle)}>Home</Link>
+                    <Link to="/services" onClick={() => setMenuToggle(!menuToggle)}>Services</Link>
+                    <Link to="/about" onClick={() => setMenuToggle(!menuToggle)}>About</Link>
+                    <Link to="/contact" onClick={() => setMenuToggle(!menuToggle)}>Contact</Link>
+                    <Link to="/order" onClick={() => setMenuToggle(!menuToggle)}>Order</Link>
+                </div>
+
             </div>
         </div>
     );
